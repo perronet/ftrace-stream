5ef10207818d6d106377d69454bf8899aaa09249
perronet <perronet@mpi-sws.org>
2021-11-11 15:29:39 +0100

    Clean build output, add new install script

--- src/binary_parser/lib/build_libs.sh
+++ src/binary_parser/lib/build_libs.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# To test the script use INSTALL_PATH=/tmp/install
+
 mkdir -p build_output
 
+# Clean previous output
 make -C libtraceevent clean
 make -C libtracefs clean
 make -C trace-cmd clean
+rm -rf build_output/*
 
 CUSTOM_PATH=$(pwd)/build_output
 
@@ -16,3 +20,8 @@ INSTALL_PATH="$CUSTOM_PATH" ../trace-cmd/make-trace-cmd.sh install
 
 cd ../trace-cmd
 INSTALL_PATH="$CUSTOM_PATH" ./make-trace-cmd.sh install_libs
+
+cd ..
+make -C libtraceevent clean
+make -C libtracefs clean
+make -C trace-cmd clean
--- src/binary_parser/lib/install.sh
+++ src/binary_parser/lib/install.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 
-# To test the script use INSTALL_PATH=/tmp/install
-
 git clone git://git.kernel.org/pub/scm/libs/libtrace/libtraceevent.git
 git clone git://git.kernel.org/pub/scm/libs/libtrace/libtracefs.git
 git clone git://git.kernel.org/pub/scm/utils/trace-cmd/trace-cmd.git
 
-make -C libtraceevent clean
-make -C libtracefs clean
-make -C trace-cmd clean
-
-cd libtraceevent
-PREFIX=/usr/local INSTALL_PATH=/ ../trace-cmd/make-trace-cmd.sh install
-
-cd ../libtracefs
-PREFIX=/usr/local INSTALL_PATH=/ ../trace-cmd/make-trace-cmd.sh install
-
-cd ../trace-cmd
-PREFIX=/usr/local INSTALL_PATH=/ ./make-trace-cmd.sh install_libs
-
-# Replace the trace-cmd.h header with a custom header.
-# The reason for this is that the standard header does not expose
-# some of the functions that we need in the binary parser.
-cd ..
-cp trace-cmd-private.h /usr/local/include/trace-cmd/trace-cmd.h
-
-make -C libtraceevent clean
-make -C libtracefs clean
-make -C trace-cmd clean
-
-sudo ldconfig
+./build_libs.sh
